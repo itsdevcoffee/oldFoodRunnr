e5a13d06f7ad6b6cf263f2727b16878af983fa13
dani frederick <dandiellie@yahoo.com>
2015-08-02 10:38:37 -0500

    Added Price to DynamicGetItemViewModel and ShoppingListRepository.

--- FoodRunnr2/FoodRunnr2/Models/DynamicGetItemViewModel.cs
+++ FoodRunnr2/FoodRunnr2/Models/DynamicGetItemViewModel.cs
@@ -11,6 +11,7 @@ namespace FoodRunnr2.Models
         public int ProductId { get; set; }
         public int ShoppingCartId { get; set; }
         public int Quantity { get; set; }
+        public decimal Price { get; set; }
         public bool IsNew { get; set; }
     }
 }
